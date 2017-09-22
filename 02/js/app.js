aa138c4275b2b4682bd016c58ad5e12fc8b4542e
= <=>
2017-09-22 17:37:27 +0900

    third

---
+++ 02/js/app.js
@@ -0,0 +1,16 @@
+// var log = document.getElementById('log');
+// console.log(log);
+// log.innerHTML = '안녕';
+
+var log = document.getElementById('log');
+console.log(log);
+
+// var a = document.querySelectorAll('#log a');
+// console.log(a, length, a[0], a[1]);
+
+
+var div = document.createElement('div');
+div.style.border = "1px solid red";
+div.innerHTML = "hello!!";
+
+document.body.appendChild(div);
